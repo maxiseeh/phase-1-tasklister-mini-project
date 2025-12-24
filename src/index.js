a1af487c0750385e60b7f10e1fcb87a0d9eb4e74
maxiseeh <maxymeeh@gmail.com>
2025-12-24 16:50:21 +0300

    initialcommit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById('create-task-form');
+  
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const task = e.target['new-task-description'].value;
+    buildToDo(task);
+  });
 });
+
+function buildToDo(task) {
+  const li = document.createElement('li');
+  li.textContent = task;
+  document.getElementById('tasks').appendChild(li);
+}
